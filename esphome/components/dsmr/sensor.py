d40f8805b7e70acbecf3ba698d5a7134220bff11
Aleksejus <aleksejus@trikdis.lt>
2023-07-20 21:34:42 +0300

    kV_2

--- esphome/components/dsmr/sensor.py
+++ esphome/components/dsmr/sensor.py
@@ -18,6 +18,7 @@ from esphome.const import (
     UNIT_KILOVOLT_AMPS_REACTIVE_HOURS,
     UNIT_KILOVOLT_AMPS_REACTIVE,
     UNIT_VOLT,
+    UNIT_KILO_VOLT,
 )
 from . import Dsmr, CONF_DSMR_ID
 
@@ -208,19 +209,19 @@ CONFIG_SCHEMA = cv.Schema(
             state_class=STATE_CLASS_MEASUREMENT,
         ),
         cv.Optional("voltage_l1"): sensor.sensor_schema(
-            unit_of_measurement="kV",
+            unit_of_measurement=UNIT_KILO_VOLT,
             accuracy_decimals=3,
             device_class=DEVICE_CLASS_VOLTAGE,
             state_class=STATE_CLASS_MEASUREMENT,
         ),
         cv.Optional("voltage_l2"): sensor.sensor_schema(
-            unit_of_measurement="kV",
+            unit_of_measurement=UNIT_KILO_VOLT,
             accuracy_decimals=3,
             device_class=DEVICE_CLASS_VOLTAGE,
             state_class=STATE_CLASS_MEASUREMENT,
         ),
         cv.Optional("voltage_l3"): sensor.sensor_schema(
-            unit_of_measurement="kV",
+            unit_of_measurement=UNIT_KILO_VOLT,
             accuracy_decimals=3,
             device_class=DEVICE_CLASS_VOLTAGE,
             state_class=STATE_CLASS_MEASUREMENT,
